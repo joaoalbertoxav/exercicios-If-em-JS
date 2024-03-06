27f6986a3f44f8112a62112a20abf8a2984a6360
joaoalbertoxav <127453239+joaoalbertoxav@users.noreply.github.com>
2024-03-06 20:36:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// // // // // // let num1 = 100
+// // // // // // let num2 = 80
+// // // // // // if (num2 === 80) {
+// // // // // //     console.log("num2 é maior que o num1")
+// // // // // // }
+// // // // // // else{
+// // // // // //     console.log("num1 é maior que o num2")
+// // // // // // }
+
+
+// // // // // let idade = 2024 - 2000
+// // // // // if (idade >= 16) {
+// // // // //     console.log("pode votar")
+// // // // // }
+// // // // // else{
+// // // // //     console.log("não pode votar")
+// // // // // }
+
+
+// // // // let senha = 1111
+// // // // if (senha === 1234) {
+// // // //     console.log("Acesso Permitido")
+// // // // }
+// // // // else{
+// // // //     console.log("Acesso Negado")
+// // // // }
+
+
+// // // let quantidade = 40
+// // // let valorvarejo = 0.30
+// // // let valoratacado = 0.25 
+// // // let compravarejo = quantidade * valorvarejo
+// // // let compraatacado = quantidade * valoratacado
+
+// // // if (quantidade >= 12) {
+// // //     console.log(` valor da compra em atacado fica ${compraatacado} reais, valor da compraem varejo fica ${compravarejo} reais´`)
+// // // }
+// // // else {
+// // //     console.log("não atendeu o requisito para ganhar o desconto")
+// // // }
+
+
+
+// // // let num1 = 10; 
+// // // let num2 = 20; 
+// // // let num3 = 30; 
+// // // if (num1 > num2 && num3) {
+// // //     console.log("Ok")
+// // // }
+// // // else if (num1 > num2 && num2 >num3){
+// // //     console.log("Ok2")
+// // // }
+// // // else if (num3 > num1 && num3 > num2) {
+// // //     console.log("Ok3")
+// // // }
+
+
+
+// // let altura = 1.86 
+// // let sexo = "masculino"
+
+// // if (sexo === "masculino") {
+// //     let pesoideal = (72.7*altura) - 58;
+// //     console.log(pesoideal);
+// // }
+// let alturaF = 1.65
+// let sexoF = "feminino"
+// if (sexoF === "feminino"){
+//     let pesoIdeal = (62.1*alturaF) - 44.7;
+//     console.log(pesoIdeal);
+// }
+
+
