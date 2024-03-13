e38ccea794e7d8fbcf2a621b98fcc9b00eb68a7f
joaoalbertoxav <127453239+joaoalbertoxav@users.noreply.github.com>
2024-03-13 19:08:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -92,3 +92,45 @@
 // }
 
 
+
+//Faça um script que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+
+  //  salários até R$ 280,00 (incluindo) : aumento de 20%
+    //salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+    //salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+    //salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+    //o salário antes do reajuste;
+    //o percentual de aumento aplicado;
+    //o valor do aumento;
+    //o novo salário, após o aumento.
+
+    let salario = Number(prompt("Digite seu salario :"))
+
+    if(salario <=280 ){
+    
+        let percentual =  1.2 - 1 
+        let novoSalario = salario*1.2
+        let valorAumento = novoSalario - salario
+        
+        alert(`O salario antes do reajuste é igual ${salario}, o percentual aplicado é de ${percentual.toFixed(2)}, o valor do aumento é de ${valorAumento} e o novo salario é de ${novoSalario}`)
+        
+    } else if(salario>280 && salario<=700) {
+        let percentual =  1.15 - 1 
+        let novoSalario = salario*1.15
+        let valorAumento = novoSalario - salario
+        
+        alert(`O salario antes do reajuste é igual ${salario}, o percentual aplicado é de ${percentual.toFixed(2)}, o valor do aumento é de ${valorAumento} e o novo salario é de ${novoSalario}`)
+    
+    } else if (salario>700 && salario <= 1500) {
+        let percentual =  1.10 - 1 
+        let novoSalario = salario*1.10
+        let valorAumento = novoSalario - salario
+        
+        alert(`O salario antes do reajuste é igual ${salario}, o percentual aplicado é de ${percentual.toFixed(2)}, o valor do aumento é de ${valorAumento} e o novo salario é de ${novoSalario}`)
+    } else if(salario>1500) {
+        let percentual =  1.05 - 1 
+        let novoSalario = salario*1.05
+        let valorAumento = novoSalario - salario
+        
+        alert(`O salario antes do reajuste é igual ${salario}, o percentual aplicado é de ${percentual.toFixed(2)}, o valor do aumento é de ${valorAumento} e o novo salario é de ${novoSalario}`)
+    }
